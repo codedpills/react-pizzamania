626cb83416b9fae4afff2f9f930d08340e59a85e
zak <codedpills@gmail.com>
2021-06-15 11:59:49 +0000

    add Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <p className="text-center">
+            {" "}
+            &copy; 2021 PizzaMania. All rights reserved
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
